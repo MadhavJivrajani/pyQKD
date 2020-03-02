7afd9f86903ee5730de009a43851127b0d660524
alastairmoody-madeye <akshata.lakshmi@gmail.com>
2020-03-02 21:47:26 +0530

    Added privacy amplification

--- errorCorrection.py
+++ errorCorrection.py
@@ -4,6 +4,7 @@
 #Find optimal length of set where the probability of finding more than 1 error is least.
 #Find parity in each of the sets. If equal, most likely the measurements are same for ALice and Bob. If different remove from list.
 #Also remove one reading from the set for which parity was announced. To maintain security (so that Eve cannot guess)
+#if block size is large enough, and parity is different, we can do bisective searching till the error is found and reject that state for both alice and bob.
 
 
 alice = ['H', 'D', 'D', 'H', 'H','H', 'D', 'D', 'H', 'H','H', 'D', 'D', 'H', 'H','H', 'D', 'D', 'H', 'H','H', 'D', 'D', 'H', 'H', 'D']
@@ -43,6 +44,8 @@ def remove_last(bits):
     bits.pop()
     return bits
 
+
+
 error, count, error_rate, alice, bob, alice_b, bob_b = calc_error_rate(alice, bob, alice_b, bob_b)
 setLen = set_length(error, count)
 j = 0
@@ -52,30 +55,66 @@ par_bob = []
 bits = []
 #m = len(alice) - len(alice)%setLen
 
-for l in range(setLen, len(alice), setLen):
-    i = l-k
-    j-=k
-    #i = alice.index(l)
-    #print len(alice), len(alice_b), setLen, alice_b[j:i], find_parity(alice_b[j:i])
-    par_alice.append(find_parity(alice_b[j:i]))
-    alice.pop(i)
-    alice_b.pop(i)
-    par_bob.append(find_parity(bob_b[j:i]))
-    bob.pop(i)
-    bob_b.pop(i)
-    j = i
-    k+=1
+def binary_search(alice_sub, bob_sub):
+#     if len(bob_sub)>1:
+#         if find_parity(alice_sub) != find_parity(bob_sub):
+#             binary_search(alice_sub[:int(len(alice_sub)/2)], bob_sub[:int(len(bob_sub)/2)])
+#             #print(alice_sub)
+#             binary_search(alice_sub[int(len(alice_sub)/2):], bob_sub[int(len(bob_sub)/2):])
+#             #print(bob_sub)
+#     elif len(bob_sub)==1:
+#         #print(alice_sub, bob_sub)
+#         #if find_parity(alice_sub)!=find_parity(bob_sub):
+#         bob_sub[0] = not bob_sub[0]
+#         return
+    r = len(alice_sub)
+    l = 0
     
-if(i!=len(alice)-1):
-    par_alice.append(find_parity(alice_b[i:len(alice)-1]))
-    alice.pop(len(alice)-1)
-    alice_b.pop(len(alice_b)-1)    
-    par_bob.append(find_parity(bob_b[i:len(bob)-1]))
-    bob.pop(len(bob)-1)
-    bob_b.pop(len(bob_b)-1)
+    while(l<r):
+        alice = alice_sub[l:r]
+        bob = bob_sub[l:r]
+        m = int((l+r)/2)
+        
+        if (find_parity(alice)!=find_parity(bob)):
+           l = m+1
+        else:
+            r = m-1
+            
+    bob_sub[m] = int(not bob_sub[m])
+    
+    return bob_sub
 
-print alice_b, bob_b
+def get_subsets(error, count, alice_b, bob_b):
+    alice, bob = [], []
+    n = len(alice_b)
+    subset_size = set_length(error, count)
+    for i in range(0, n, subset_size):
+        alice.append(alice_b[i:i+subset_size])
+        bob.append(bob_b[i:i+subset_size])
+        
+    return alice, bob
 
+def error_correction(alice_b, bob_b, error, count):
+    alice_sub, bob_sub = [], []
+    alice_sub, bob_sub = get_subsets(error, count, alice_b, bob_b)
+    #print(alice_sub, bob_sub)
+    par_alice, par_bob = [], []
+    for i in range(len(alice_sub)):
+        par_alice.append(find_parity(alice_sub[i]))
+        alice_sub[i].pop(0)
+        par_bob.append(find_parity(bob_sub[i]))
+        bob_sub[i].pop(0)
+    print(alice_sub, bob_sub) 
+    corrected_alice, corrected_bob = [], []
+    for i in range(len(par_alice)):
+        if par_alice[i]==par_bob[i]:
+            corrected_bob.append(bob_sub[i])
+        else:
+            corrected_bob.append(binary_search(alice_sub[i], bob_sub[i]))
+        corrected_alice.append(alice_sub[i])
+    return corrected_alice, corrected_bob
+    
+#--------------------------------------------------------------------#--------------------------------------------------------------------------------------#
 
 #Selecting set length
 #Formula: n-k-s
@@ -84,5 +123,44 @@ print alice_b, bob_b
 #s: security parameter
 #parities of these subsets becomes final key
 
-k = int(2*error) + 1
-n = len(alice_b)
+
+
+def privacy_amplification(n, error_rate, s, alice_bit, bob_bit):
+    k = int(error_rate * 2) 
+    subset_size = n - k - s
+    final_alice = []
+    final_bob = []
+    
+    alice_b = []
+    bob_b = []
+    
+    alice_subsets = []
+    bob_subsets = []
+    
+    for i in alice_bit:
+        for j in i:
+            alice_b.append(j)
+    
+    for i in bob_bit:
+        for j in i:
+            bob_b.append(j)
+    
+    for i in range(0, n, subset_size):
+        alice_subsets.append(alice_b[i:i+subset_size])
+        bob_subsets.append(bob_b[i:i+subset_size])
+        
+    bob_parity = 0
+    alice_parity = 0
+    
+    #calculate parities of sets and compare and eliminate if parities dont match
+    for i in range(len(alice_subsets)):
+        
+        alice = find_parity(alice_subsets[i])
+        bob = find_parity(bob_subsets[i])
+                
+        if alice == bob:
+            final_alice.append(alice)
+            final_bob.append(bob)
+    
+    return final_alice, final_bob
+
